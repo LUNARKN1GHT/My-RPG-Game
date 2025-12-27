c568b48e9e2ce1396a213387e174146314f8a810
IVANLEE <lee162286@gmail.com>
2025-12-27 11:30:55 +0800

    增加技能基类

---
+++ include/skill/Skill.h
@@ -0,0 +1,30 @@
+#ifndef SKILL_H
+#define SKILL_H
+
+#include <string>
+
+class Character; // 前向声明，避免头文件循环依赖
+
+/**
+ * @brief 技能基类（抽象类
+ */
+class Skill {
+private:
+    std::string name_; // 技能的名称
+
+public:
+    // 析构函数，允许子类改写
+    virtual ~Skill() = default;
+
+    // 接口函数，获取技能名称
+    virtual std::string getName() const = 0;
+
+    /**
+     * @brief 使用技能
+     * @param user 技能使用者
+     * @param target 技能目标
+     */
+    virtual void use(Character& user, Character& target) = 0;
+};
+
+#endif // SKILL_H
---
+++ src/skill/Skill.cpp
@@ -0,0 +1,3 @@
+#include "../include/skill/Skill.h"
+
+Skill::~Skill() = default; // 析构函数定义
